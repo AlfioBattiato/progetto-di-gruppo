bedc4f244be9795df8485270d3743e81b1c83e02
ALFIO BATTIATO <battiato3@gmail.com>
2024-02-06 19:36:59 +0100

    feedback page complete

--- js/feedback.js
+++ js/feedback.js
@@ -1,6 +1,6 @@
 const divStars = document.getElementsByClassName("container-stars")[0];
 const star = document.getElementById("star");
-
+// funzione per creare le stelle in modo dinamico
 function creaStar() {
   for (let i = 0; i < 9; i++) {
     /* console.log(star); */
@@ -9,3 +9,20 @@ function creaStar() {
   }
 }
 creaStar();
+
+//funzione che cambia colore alle mie stelle se cliccate
+const divStars2 = divStars;
+
+Array.from(divStars2.children).forEach((e, index) => {
+  e.onclick = function () {
+    Array.from(divStars2.children).forEach((child) => {
+      // child.style.removeProperty('color');
+      child.style.color = "hsl(233 60% 18% / 1)";
+
+    });
+    for (let i = 0; i <= index; i++) {
+      divStars2.children[i].style.color = " #00ffff";
+    }
+  };
+});
+// console.log(divStars2)

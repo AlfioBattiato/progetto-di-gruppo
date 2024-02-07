6fbc78471da16ae04611b81e5ca980d0d89681b6
Marco Pala <155745769+marcopa94@users.noreply.github.com>
2024-02-07 21:45:36 +0100

    result agggiustata-spazio svg

--- js/feedback.js
+++ js/feedback.js
@@ -11,6 +11,7 @@ creaStar();
 let starValore = 0;
 //funzione che cambia colore alle mie stelle se cliccate
 divStars = Array.from(divStars.children);
+let divEmo = document.getElementsByClassName("svgContainer")[0];
 
 divStars.forEach((el, index) => {
   //click
@@ -19,6 +20,13 @@ divStars.forEach((el, index) => {
     console.log(starValore);
     divStars.forEach((child) => {
       child.classList.remove("acceso");
+      if (starValore <= 4) {
+        console.log("pessimo");
+      } else if (starValore > 4 && starValore < 8) {
+        console.log("buono");
+      } else if (starValore >= 8) {
+        console.log("ottimo");
+      }
     });
 
     for (let i = 0; i <= index; i++) {

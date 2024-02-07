5f70f73a37bb0011d58004afecc69af7ade2dc8b
Marco Pala <155745769+marcopa94@users.noreply.github.com>
2024-02-07 15:25:17 +0100

    buttone

--- js/welcome.js
+++ js/welcome.js
@@ -1,13 +1,15 @@
 const checkbox = document.getElementById("checkbox");
 checkbox.addEventListener("change", validationCheck);
 const a = document.getElementById("a");
-console.log(a);
+
 function validationCheck(e) {
+  let button = document.getElementsByClassName("button")[0];
+
   if (e.target.checked) {
     a.setAttribute("href", "../html/benchmark.html");
+    button.style.opacity = "1";
   } else {
-    a.setAttribute("href", "");
+    a.setAttribute("href", "#");
+    button.style.opacity = "0.5";
   }
-
-  console.log(a);
 }

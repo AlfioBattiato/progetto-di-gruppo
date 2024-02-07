18c7e6a36b06c99e8fc42663481c7935e7a4e51d
ALFIO BATTIATO <battiato3@gmail.com>
2024-02-07 12:58:11 +0100

    feedback end

--- js/feedback.js
+++ js/feedback.js
@@ -1,9 +1,8 @@
-const divStars = document.getElementsByClassName("container-stars")[0];
+let divStars = document.getElementsByClassName("container-stars")[0];
 const star = document.getElementById("star");
 // funzione per creare le stelle in modo dinamico
 function creaStar() {
   for (let i = 0; i < 9; i++) {
-    /* console.log(star); */
     let clonedStar = star.cloneNode(true);
     divStars.appendChild(clonedStar);
   }
@@ -11,18 +10,36 @@ function creaStar() {
 creaStar();
 
 //funzione che cambia colore alle mie stelle se cliccate
-const divStars2 = divStars;
+divStars = Array.from(divStars.children);
 
-Array.from(divStars2.children).forEach((e, index) => {
-  e.onclick = function () {
-    Array.from(divStars2.children).forEach((child) => {
-      // child.style.removeProperty('color');
-      child.style.color = "hsl(233 60% 18% / 1)";
-
+divStars.forEach((el, index) => {
+  //click
+  el.onclick = function () {
+    divStars.forEach((child) => {
+      child.classList.remove("acceso");
     });
+
     for (let i = 0; i <= index; i++) {
-      divStars2.children[i].style.color = " #00ffff";
+      divStars[i].classList.add("acceso");
     }
   };
+
+  //nohover
+  el.addEventListener("mouseout", function () {
+    divStars.forEach((child) => {
+      child.classList.remove("isHover");
+    });
+  });
+  //ishover
+  el.addEventListener("mouseover", function () {
+    divStars.forEach((child) => {
+      child.classList.remove("isHover");
+    });
+
+    for (let i = 0; i <= index; i++) {
+      if (!divStars[i].classList.contains("acceso")) {
+        divStars[i].classList.add("isHover");
+      }
+    }
+  });
 });
-// console.log(divStars2)
